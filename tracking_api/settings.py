72e039453180f024984e2f30700efb3f9189bb1e
jawahar-dev <jawahardev1995@gmail.com>
2024-10-24 14:21:33 +0530

    added dockerfile

--- tracking_api/settings.py
+++ tracking_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8=c%pu2j20c))$hu7ev(^z)tg=o05&+srila8#bn^-c@1_(nk*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
